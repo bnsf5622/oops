6a0619e46c1bac05788b5da7382a80912f766b18
Chuck <bnsf5622@gmail.com>
2020-03-30 15:22:49 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace OOPSConcepts
+{
+    class MainClass
+    {
+    public void Student()
+    {
+        int noOfStudents = 50;
+        Console.WriteLine("No of Students in the Class is " +
+        noOfStudents);
+    }
+        static void Main(string[] args)
+    {
+        MainClass objMainClass = new MainClass();
+        objMainClass.Student();
+        Console.ReadLine();
+    }
+}
+}
